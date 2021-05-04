fcd954f7d09695a328395906e2e97c3e97567109
Scott Warchal <scott.warchal@crick.ac.uk>
2021-05-04 15:11:53 +0100

    Variant (#11)
    * get variant from barcode and add col to db (towards #6)
    
    * add tests, option to overwrite existing db
    
    * fix logging messages
    
    * created_plate list filter by variant
    
    * tidy up
    
    * improve docs
    
    * retry for BlockingIOError
    
    * remove 96-well plate stuff
    
    * add cli tool to remove processed workflows
    
    * update readme
    
    * deprecate 96 well functions
    
    * update concurrency in README example
    
    * add remove_stitched cli tool
    
    * update readme for remove_stitched
    
    * add slack alerts for failed jobs

--- README.md
+++ README.md
@@ -21,13 +21,13 @@ This requires an installation of redis-server, celery and a MySQL driver.
 ## To run:
 1. Start redis  
 ```
-redis-server
+redis-server --port 7777
 ```
 
 2. Start celery in the working directory  
 ```
-celery -A task worker -Q analysis --concurrency=4 --loglevel=INFO -E -n analysis
-celery -A task worker -Q image_stitch --concurrency=1 --loglevel=INFO -E -n image_stitcher
+celery -A task worker -Q analysis --concurrency=1 --loglevel=INFO -E -n analysis
+celery -A task worker -Q image_stitch --concurrency=8 --loglevel=INFO -E -n image_stitcher
 ```
 
 3. Start watchdog to monitor filesystem  
@@ -43,12 +43,34 @@ celery flower -A task --address=0.0.0.0 --port=5555 --basic_auth={username}:{pas
 
 ## To re-analyse previously run jobs:
 Analysis jobs are stored in a local sqlite database to stop duplicate entries
-into the LIMS. To re-analyse anything (in case of an error before LIMS upload) you have to
-delete that entry from `processed_experiments.sqlite`
+into the LIMS. To re-analyse anything (in case of an error before LIMS upload) you have to delete that entry from `processed_experiments.sqlite`
 
-e.g if you want to delete the experiment "000EXAMPLE"
+### CLI tool
+This will remove workflow ID 101 from the `processed` table (all variants).
+```bash
+./remove_processed 101
+```
+
+To specify the variant:
+```bash
+./remove_processed 101 --variant a
+```
+```bash
+./remove_processed 101 -v a b
+```
+
+To remove stitched entries (all variants):
+```bash
+./remove_stitched 101
+```
+
+
+### Manually
+
+e.g if you want to delete the workflow_id "000101"
 ```
 sqlite3 processed_experiments.sqlite
 
-sqlite> DELETE FROM processed WHERE experiment="000EXAMPLE"
+sqlite> DELETE FROM processed WHERE experiment="000101";
+sqlite> DELETE FROM stitched WHERE plate_name LIKE "%000101";
 ```
--- task.py
+++ task.py
@@ -1,33 +1,78 @@
+import os
 import time
 from urllib.error import URLError
+import textwrap
 
-from celery import Celery
+import celery
+import requests
 
 import plaque_assay
 import stitch_images
 
 
 REDIS_PORT = 7777
+SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_NEUTRALISATION")
 
-celery = Celery(
+
+celery = celery.Celery(
     "task",
     backend=f"redis://localhost:{REDIS_PORT}/0",
     broker=f"redis://localhost:{REDIS_PORT}/0"
 )
 
 
+class BaseTask(celery.Task):
+
+    @staticmethod
+    def slack_alert(exc, task_id, args, kwargs, einfo):
+        """send slack message on failure"""
+        data = {
+            "text": "Something broke",
+            "username": "NE analysis",
+            "attachments": [
+                {
+                    "text": textwrap.dedent(f"""
+                        :fire: OH NO! :fire:
+                        #####################################
+                        {task_id!r} failed
+                        #####################################
+                        args: {args!r}
+                        #####################################
+                        {einfo!r}
+                        #####################################
+                        {exc!r}
+                        #####################################
+                    """),
+                    "color": "#ad1721",
+                    "attachment_type": "default",
+                }
+            ]
+        }
+        r = requests.post(SLACK_WEBHOOK_URL, json=data)
+        return r.status_code
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        """custom actions on task failure"""
+        print("sending slack alert")
+        status_code = self.slack_alert(exc, task_id, args, kwargs, einfo)
+        if status_code != 200:
+            print(f"{status_code}: failed to send slack alert")
+
+
 @celery.task(
     queue="analysis",
+    base=BaseTask,
     autoretry_for=(ConnectionResetError, FileNotFoundError, BlockingIOError)
 )
 def background_analysis_384(plate_list):
     """check for new experiment directory"""
     time.sleep(10)
-    plaque_assay.main.run(plate_list, plate=384)
+    plaque_assay.main.run(plate_list)
 
 
 @celery.task(
     queue="image_stitch",
+    base=BaseTask,
     autoretry_for=(
         ConnectionResetError, FileNotFoundError, URLError, BlockingIOError
     )
